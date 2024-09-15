d675374c103212b027f272d59ea89a874c06ad2b
tamathecxder <yudistiraeka.pratama012@gmail.com>
2024-09-15 19:09:13 +0700

    if expressions

--- app/src/main/kotlin/org/example/App.kt
+++ app/src/main/kotlin/org/example/App.kt
@@ -63,5 +63,18 @@ fun main() {
 
     val name = "Kotlin"
     print("My name is $name")
+
+    val openHours = 7
+    val now = 7
+    val office: String
+    office = if (now > 7) {
+        "Office already open"
+    } else if (now == openHours){
+        "Wait a minute, office will be open"
+    } else {
+        "Office is closed"
+    }
+
+    print(office)
 }
 
