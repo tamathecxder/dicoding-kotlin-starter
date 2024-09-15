69daab207e7e8d858d3d5251d4200be1bb32aa99
tamathecxder <yudistiraeka.pratama012@gmail.com>
2024-09-15 19:48:56 +0700

    null in kotlin

--- app/src/main/kotlin/org/example/App.kt
+++ app/src/main/kotlin/org/example/App.kt
@@ -88,5 +88,12 @@ fun main() {
     intArray[2] = 11                       // [1, 3, 11, 7]
 
     print(intArray[2])
+
+    var obj: String? = "Dicoding"
+
+    if(obj is String) {
+        // Tidak membutuhkan casting secara eksplisit.
+        println("String length is ${obj.length}")
+    }
 }
 
