0b319c06698a8580a1ca465f4ff955012fd83e4e
tamathecxder <yudistiraeka.pratama012@gmail.com>
2024-09-15 19:08:00 +0700

    string datatype

--- app/src/main/kotlin/org/example/App.kt
+++ app/src/main/kotlin/org/example/App.kt
@@ -44,4 +44,21 @@ fun main() {
     println("Vocal " + vocal--)
     println("Vocal " + vocal--)
     println("Vocal " + vocal--)
+
+    val text  = "Kotlin"
+    for (char in text){
+        print("$char ")
+    }
+
+    val statement = "Kotlin is \"Awesome!\""
+
+    val line = """
+        Line 1
+        Line 2
+        Line 3
+        Line 4
+    """.trimIndent()
+
+    print(line)
 }
+
