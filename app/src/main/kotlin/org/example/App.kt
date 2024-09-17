14eaaedfe18004b7d9b95070c7bef2c2c4b8e34a
tamathecxder <yudistiraeka.pratama012@gmail.com>
2024-09-17 14:19:47 +0700

    break and continue labels

--- app/src/main/kotlin/org/example/App.kt
+++ app/src/main/kotlin/org/example/App.kt
@@ -229,5 +229,23 @@ fun main() {
 
     val rangeChar = 'A'.rangeTo('F')
 
+    // Break and Continue
+    val listOfInt = listOf(1, 2, 3, null, 5, null, 7)
+
+    for (i in listOfInt) {
+        if (i == 1) continue
+        if (i == null) break
+        print(i)
+    }
+
+    // Labels
+    loop@ for (i in 1..10) {
+        println("Outside Loop")
+
+        for (j in 1..10) {
+            println("Inside Loop")
+            if ( j > 5) break@loop
+        }
+    }
 }
 
