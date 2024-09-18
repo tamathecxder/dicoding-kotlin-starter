fd18d031ea043ad8f9bebe4b9482b2871c2a9331
tamathecxder <yudistiraeka.pratama012@gmail.com>
2024-09-18 21:16:08 +0700

    error handling

--- app/src/main/kotlin/org/example/OOP.kt
+++ app/src/main/kotlin/org/example/OOP.kt
@@ -211,4 +211,36 @@ fun main() {
     println(factorial(4.0))
     println(pow(3.0, 2.0))
     println(PI)
+
+    // Error Handling
+    val someNullValue: String? = null
+    lateinit var someMustNotNullValue: String
+
+    try {
+        someMustNotNullValue = someNullValue!!
+        println(someMustNotNullValue)
+    } catch (e: Exception) {
+        someMustNotNullValue = "Nilai String Null"
+        println(someMustNotNullValue)
+    } finally {
+        println(someMustNotNullValue)
+    }
+
+    // Multiple Catch blocks
+    val someStringValue: String? = null
+    var someIntValue: Int = 0
+
+    try {
+        someIntValue = someStringValue!!.toInt()
+    } catch (e: NullPointerException) {
+        someIntValue = 0
+    } catch (e: NumberFormatException) {
+        someIntValue = -1
+    } finally {
+        when(someIntValue){
+            0 -> println("Catch block NullPointerException terpanggil !")
+            -1 -> println("Catch block NumberFormatException terpanggil !")
+            else -> println(someIntValue)
+        }
+    }
 }
