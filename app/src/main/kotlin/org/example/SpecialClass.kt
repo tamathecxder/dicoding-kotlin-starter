413a98817bfafaa010fd7ca8970689bb26c9d432
tamathecxder <yudistiraeka.pratama012@gmail.com>
2024-09-21 11:34:57 +0700

    enumeration

--- app/src/main/kotlin/org/example/SpecialClass.kt
+++ app/src/main/kotlin/org/example/SpecialClass.kt
@@ -17,6 +17,26 @@ class House {
     }
 }
 
+enum class Color(val value: Int) {
+    RED(0xFF0000){
+        override fun printValue() {
+            println("value of RED is $value")
+        }
+    },
+    GREEN(0x00FF00){
+        override fun printValue() {
+            println("value of GREEN is $value")
+        }
+    },
+    BLUE(0x0000FF){
+        override fun printValue() {
+            println("value of BLUE is $value")
+        }
+    };
+
+    abstract fun printValue()
+}
+
 fun main() {
     val user = User("Asep", 20)
     val dataUser = DataUser("nrohmen", 17)
@@ -41,4 +61,25 @@ fun main() {
     val house = House()
     val room = house.Room()
     room.measureRoomArea()
+
+    val colorRed = Color.RED
+    val colorGreen = Color.GREEN
+    val colorBlue = Color.BLUE
+
+    val colors: Array<Color> = Color.values()
+    colors.forEach { color ->
+        print("$color ")
+    }
+
+    val color: Color = Color.valueOf("RED")
+    println("Color is $color")
+    println("Color value is ${color.value.toString(16)}")
+
+    print("Position is ${color.ordinal}")
+
+    when(color){
+        Color.RED -> print("Color is Red")
+        Color.BLUE -> print("Color is Blue")
+        Color.GREEN -> print("Color is Green")
+    }
 }
