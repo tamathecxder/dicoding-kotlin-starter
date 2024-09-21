4f328d964ce8071ef8131022d6861689e9990ceb
tamathecxder <yudistiraeka.pratama012@gmail.com>
2024-09-21 20:35:30 +0700

    collection - set

--- app/src/main/kotlin/org/example/Collection.kt
+++ app/src/main/kotlin/org/example/Collection.kt
@@ -6,5 +6,23 @@ fun main() {
 
     println(numberList[0])
     println(charList)
+
+    val setA = setOf(1, 2, 4, 2, 1, 5)
+    val setB = setOf(1, 2, 4, 5)
+    println(setA == setB)
+    print(5 in setA)
+
+    val setC = setOf(1, 5, 7)
+    val union = setA.union(setC)
+    val intersect = setA.intersect(setC)
+
+    println(union)
+    println(intersect)
+
+    // mutable
+    val mutableSet = mutableSetOf(1, 2, 4, 2, 1, 5)
+//mutableSet[2] = 6 // tidak bisa mengubah mutableSet
+    mutableSet.add(6) // menambah item di akhir set
+    mutableSet.remove(1) //menghapus item yang memiliki nilai 1
 }
 
