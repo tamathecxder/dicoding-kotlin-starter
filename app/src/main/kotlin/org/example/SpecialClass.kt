c0be3f69464d7ffa892d1859e7fe9143aa6e2f2d
tamathecxder <yudistiraeka.pratama012@gmail.com>
2024-09-21 10:34:50 +0700

    desctructuring data class

--- app/src/main/kotlin/org/example/SpecialClass.kt
+++ app/src/main/kotlin/org/example/SpecialClass.kt
@@ -7,7 +7,7 @@ data class DataUser(val name : String, val age : Int)
 fun main() {
     val user = User("Asep", 20)
     val dataUser = DataUser("nrohmen", 17)
-    val dataUser2 = DataUser("nrohmen", 17)
+    val dataUser2 = DataUser("Agus", 19)
     val dataUser3 = DataUser("dimas", 24)
     val dataUser4 = dataUser.copy()
     val dataUser5 = dataUser.copy(age = 18)
@@ -17,4 +17,11 @@ fun main() {
     println(dataUser2.equals(dataUser3))
     println(dataUser4)
     println(dataUser5)
+
+    val name = dataUser.component1()
+    val age = dataUser.component2()
+    val (name2, age2) = dataUser2
+
+    println("My name is $name, I am $age years old")
+    println("My name is $name2, I am $age2 years old")
 }
