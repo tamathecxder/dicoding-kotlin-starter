ece7bf01929f0363ab0404eff88f71340483735a
tamathecxder <yudistiraeka.pratama012@gmail.com>
2024-09-22 20:17:41 +0700

    fp - kotlin standard library

--- app/src/main/kotlin/org/example/FP.kt
+++ app/src/main/kotlin/org/example/FP.kt
@@ -8,6 +8,9 @@ inline fun printResult(value: Int, sum: (Int) -> Int) {
     println(result)
 }
 
+class PersonV2(val name: String, val age: Int) {
+}
+
 fun main() {
     val sum: Arithmetic = { valueA, valueB -> valueA + valueB }
 
@@ -64,4 +67,47 @@ fun main() {
     }
 
     println(messages)
+
+    // receiver "this"
+    val buildString = StringBuilder().apply {
+        append("Hello ")
+        append("Kotlin ")
+    }
+
+    // receiver "it"
+    val text = "Hello"
+    text.let {
+        val message = "$it Kotlin"
+        println(message)
+    }
+
+    // apply: Menginisialisasi objek dan mengatur propertinya, kemudian mengembalikan objek tersebut
+    val personApply = PersonV2("John", 25).apply {
+        println("apply: Initializing $name, age $age")
+    }
+
+    // also: Melakukan tindakan tambahan pada objek dan mengembalikan objek tersebut
+    val personAlso = PersonV2("Jane", 30).also {
+        println("also: Initializing ${it.name}, age ${it.age}")
+    }
+
+    // let: Melakukan operasi pada objek dan mengembalikan hasil operasi tersebut
+    val personLet = PersonV2("Bob", 22).let {
+        println("let: Initializing ${it.name}, age ${it.age}")
+        "Result: ${it.name} is ${it.age} years old" // return value
+    }
+    println("let result: $personLet")
+
+    // run: Menjalankan blok kode pada objek dan mengembalikan hasil blok kode tersebut
+    val personRun = PersonV2("Alice", 28).run {
+        println("run: Initializing $name, age $age")
+        "Result: $name is $age years old" // return value
+    }
+    println("run result: $personRun")
+
+    // with: Menggunakan objek sebagai receiver untuk blok kode dan mengembalikan hasil blok kode tersebut
+    val personWithResult = with(PersonV2("Charlie", 35)) {
+        println("with: Initializing $name, age $age")
+        "Result: $name is $age years old" // return value
+    }
 }
