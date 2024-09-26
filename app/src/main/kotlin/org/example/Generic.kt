e4828884577fd180fbb449362706e3509d0247fc
tamathecxder <yudistiraeka.pratama012@gmail.com>
2024-09-26 16:37:42 +0700

    generic - declare generic function

--- app/src/main/kotlin/org/example/Generic.kt
+++ app/src/main/kotlin/org/example/Generic.kt
@@ -15,3 +15,12 @@ class ArrayList<T> : List<T>{
         return this[index]
     }
 }
+
+//public fun <T> List<T>.slice(indices: Iterable<Int>): List<T> {
+//    /*...*/
+//}
+
+fun main() {
+    val numbers = (1..100).toList()
+    print(numbers.slice<Int>(1..10))
+}
