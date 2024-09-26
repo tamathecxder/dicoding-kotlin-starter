c7dd4b3b9b648dd40bb4c7c5cf2ca22b70935d08
tamathecxder <yudistiraeka.pratama012@gmail.com>
2024-09-26 16:51:40 +0700

    generic - covariant, contravariant

--- app/src/main/kotlin/org/example/Generic.kt
+++ app/src/main/kotlin/org/example/Generic.kt
@@ -30,6 +30,34 @@ class ListNumber<T : Number>(private val items: List<T>) : List<T> {
     }
 }
 
+abstract class VehicleV2(wheel: Int)
+class Car(speed: Int) : VehicleV2(4)
+class MotorCycle(speed: Int) : VehicleV2(2)
+
+// Covariant
+interface ListV2<out E> : Collection<E> {
+    operator fun get(index: Int): E
+}
+
+// Contravariant
+interface Comparable<in T> {
+    operator fun compareTo(other: T): Int
+}
+
+interface Binatang {}
+
+class Kandang<out T : Binatang>(private val hewan: T) {
+    fun dapatkanHewan(): T {
+        return hewan
+    }
+}
+
+class PenjagaKandang<in T : Binatang> {
+    fun urusKandang(hewan: T) {
+        // Melakukan sesuatu dengan kandang yang berisi hewan
+    }
+}
+
 fun main() {
     val numbers = (1..100).toList()
     print(numbers.slice<Int>(1..10))
@@ -37,4 +65,18 @@ fun main() {
 //    val numbers1 = ListNumber<Long>()
 //    val numbers2 = ListNumber<Int>()
 //    val numbers3 = ListNumber<String>() // err: Type argument is not within its bounds
+
+    val car = Car(200)
+    val motorCycle = MotorCycle(100)
+    var vehicle: VehicleV2 = car
+    vehicle = motorCycle
+
+    val carList = listOf(Car(100) , Car(120))
+    val vehicleList = carList
+
+//    val kandangKucing: Kandang<Cat> = Kandang(Cat())
+//    val kandangBinatang: Kandang<Binatang> = kandangKucing // Covariant, dapat di-substitute
+//
+//    val penjagaBinatang: PenjagaKandang<Binatang> = PenjagaKandang<Binatang>()
+//    val penjagaKucing: PenjagaKandang<Kucing> = penjagaBinatang // Contravariant, dapat di-substitute
 }
