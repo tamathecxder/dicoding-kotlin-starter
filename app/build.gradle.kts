31033c915c554c8e42e3cd2da09ec3fb6e3684cb
tamathecxder <yudistiraeka.pratama012@gmail.com>
2024-09-29 10:01:05 +0700

    coroutine - init

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -29,6 +29,8 @@ dependencies {
 
     // This dependency is used by the application.
     implementation(libs.guava)
+
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.5.1")
 }
 
 // Apply a specific Java toolchain to ease working on different environments.
---
+++ app/src/main/kotlin/org/example/Coroutine.kt
@@ -0,0 +1,12 @@
+package org.example
+
+import kotlinx.coroutines.*
+
+fun main() = runBlocking{
+    launch {
+        delay(1000L)
+        println("Coroutines!")
+    }
+    println("Hello,")
+    delay(2000L)
+}
