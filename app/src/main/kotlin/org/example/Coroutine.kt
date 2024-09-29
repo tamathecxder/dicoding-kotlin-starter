3256fe4f113be41bc394dfe441edaca74a3f6b6a
tamathecxder <yudistiraeka.pratama012@gmail.com>
2024-09-29 10:04:33 +0700

    coroutine - coroutine builder async

--- app/src/main/kotlin/org/example/Coroutine.kt
+++ app/src/main/kotlin/org/example/Coroutine.kt
@@ -1,12 +1,40 @@
 package org.example
 
 import kotlinx.coroutines.*
+import kotlin.system.measureTimeMillis
 
-fun main() = runBlocking{
-    launch {
-        delay(1000L)
-        println("Coroutines!")
+//fun main() = runBlocking{
+//    launch {
+//        delay(1000L)
+//        println("Coroutines!")
+//    }
+//    println("Hello,")
+//    delay(2000L)
+//}
+
+suspend fun getCapital(): Int {
+    delay(1000L)
+    return 50000
+}
+
+suspend fun getIncome(): Int {
+    delay(1000L)
+    return 75000
+}
+
+fun main() = runBlocking {
+    val timeOne = measureTimeMillis {
+        val capital = getCapital()
+        val income = getIncome()
+        println("Your profit is ${income - capital}")
     }
-    println("Hello,")
-    delay(2000L)
+
+    val timeTwo = measureTimeMillis {
+        val capital = async { getCapital() }
+        val income = async { getIncome() }
+        println("Your profit is ${income.await() - capital.await()}")
+    }
+
+    println("Completed in $timeOne ms vs $timeTwo ms")
+
 }
